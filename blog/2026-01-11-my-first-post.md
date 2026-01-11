80dea3a543d84a38b4f6402452e69eea929b4258
Jack Chuang <chuangyc@Sellys-MacBook-Pro.local>
2026-01-11 22:29:13 +0800

    Add new blog post: My First Blog Post

---
+++ blog/2026-01-11-my-first-post.md
@@ -0,0 +1,19 @@
+---
+slug: my-first-post
+title: My First Blog Post
+authors: [chuangyc]
+tags: [blog, docusaurus]
+---
+
+Hello world 👋
+
+This is my first post on **chuangyc.github.io**.
+
+## Why I built this blog
+
+- Personal notes
+- Tech experiments
+- Learning in public
+
+See you in the next post 🚀
+
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,16 +20,16 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://chuangyc.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'chuangyc', // Usually your GitHub org/user name.
+  projectName: 'chuangyc.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
